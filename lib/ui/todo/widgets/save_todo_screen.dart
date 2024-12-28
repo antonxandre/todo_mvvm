986ea75cd8a5051bfc41603573302a332fbda9e6
Antonio Andrade Gomes Júnior <gomesmax1997@gmail.com>
2024-12-27 22:34:17 -0300

    feat: save todo

--- lib/ui/todo/widgets/save_todo_screen.dart
+++ lib/ui/todo/widgets/save_todo_screen.dart
@@ -4,13 +4,52 @@ import 'package:todo_mvvm/ui/todo/view_model/save_todo_view_model.dart';
 class SaveTodoScreen extends StatelessWidget {
   final SaveTodoViewModel viewModel;
 
-  const SaveTodoScreen({super.key, required this.viewModel});
+  const SaveTodoScreen({
+    super.key,
+    required this.viewModel,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Create a task'),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Create a task',
+          ),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            spacing: 10,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Title',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Description',
+                ),
+                maxLines: null,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
